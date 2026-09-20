81ac46ddab5c6d0b6171c5122e6f0be21e406c7f
fxrebermanagement-star <fxrebermanagement@gmail.com>
2026-09-20 13:02:04 +0200

    chore: app-loader expects 16 app parts

--- js/app-loader.js
+++ js/app-loader.js
@@ -1,7 +1,7 @@
 /** Assembles UNIVERSUM app.js from parts then runs it. */
 (function () {
   var s = "";
-  var n = 8;
+  var n = 16;
   for (var i = 0; i < n; i++) {
     var p = window["__APP_PART_" + i + "__"];
     if (typeof p !== "string") {
